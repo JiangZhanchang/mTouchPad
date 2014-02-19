cf7fbb13fb4ae4bded0b83b0608987631880c6c6
JiangZhanchang <jiangzhanchang@gmail.com>
2014-02-19 10:26:33 +0800

    Initial commit.

---
+++ src/com/moonspringstudio/mTouchPad/WelcomePage.java
@@ -0,0 +1,15 @@
+package com.moonspringstudio.mTouchPad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomePage extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
